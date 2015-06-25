ec1db6aad2148d9cf4de77085b0e3f0601159c3a
Tariq <csetariq@gmail.com>
2015-06-25 10:59:47 +0530

    final version. no zip handling

--- utils/uploadMerge.pl
+++ utils/uploadMerge.pl
@@ -1,11 +1,14 @@
 #!/usr/local/bin/perl
 
 use File::Copy qw(copy);
+my $mergeCount = 0;
+my $zipCount = 0;
+my $notCopied = 0;
 
 sub mergeFiles {
     my ($src, $dst) = @_;
     
-    print "Gonna merge $src into $dst\n";
+    print "\nMerging $src\ninto    $dst\n";
     
     open(SRC, $src) or die "not able to open $src for reading\n";
     open(DST, ">>", $dst) or die "not able to open $dst for appending\n";
@@ -16,16 +19,23 @@ sub mergeFiles {
     }
     close(SRC);
     close(DST);
+    $mergeCount += 1;
+}
+
+sub usage {
+    print "Usage:\n\n";
+    print "uploadMerge.pl /path/to/dir/that/has/new/devices /path/to/dir/of/original/upload\n";
 }
 
 my $leftDir;
 my $rightDir;
 
 if (scalar(@ARGV) < 2) {
-    die "need left and right directory names to merge\n";
+    usage();
+    exit(1);
 } else {
     ($leftDir, $rightDir) = @ARGV;
-    print "Got cmd line args $leftDir, $rightDir\n";
+    print "Got command-line args $leftDir, $rightDir\n";
 }
 
 opendir(LDIR, $leftDir) or die "not able to open $leftDir for reading\n";
@@ -34,8 +44,23 @@ while (my $file = readdir(LDIR)) {
     next unless ($file =~ "download|zip");
     $leftFile = File::Spec->catpath("", $leftDir, $file);
     $rightFile = File::Spec->catpath("", $rightDir, $file);
-    mergeFiles($leftFile, $rightFile) if ($file =~ "download");
-    copy ($rightFile, $rightFile) if ($file =~ 'zip$');
+    mergeFiles($leftFile, $rightFile) if ($file =~ 'download$');
+    if ($file =~ 'zip$') {
+        if (not -f $rightFile) {
+            $totCount += 1;
+            copy($leftFile, $rightFile);
+        } elsif (-f $rightFile) {
+            print STDERR "\nFile $rightFile already exists! not copied!\n";
+            $notCopied += 1;
+        }
+    } 
+    
 }
+print "\n\n";
+print "Merged $mergeCount download file(s)\n";
+print "Copied $zipCount zip file(s)\n";
+print "Not copied $notCopied zip file(s)\n";
+print "\n";
+print "Compress \"$rightDir\" directory back to zip archive\n";
 
 closedir(LDIR);
