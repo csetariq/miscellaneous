c01fe5a7818ab67deead0c02ecdb3c09d26e061a
Tariq <csetariq@gmail.com>
2016-01-11 00:26:42 +0530

    people kill problem for cmd line input

--- hackerearth/circlekill/Main.java
+++ hackerearth/circlekill/Main.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String... args) {
+        int N = (args.length == 0) ? 100 : Integer.parseInt(args[0]);
         Queue<Integer> q = new LinkedList<Integer>();
-        for (int i = 1; i <= 100; ++i)
+        for (int i = 1; i <= N; ++i)
             q.offer(i);
         while (q.size() != 1) {
             q.offer(q.poll());
-            System.out.print(q.poll() +" ");
+//            System.out.print(q.poll() +" ");
+            q.poll();
         }
         System.out.println(q.poll());
     }
