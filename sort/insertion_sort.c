19b8ecebf157b173702cfc8cd7527e8218fd8ef4
Tariq <csetariq@gmail.com>
2016-01-14 00:18:41 +0530

    add insertion sort

---
+++ sort/insertion_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int is_sorted(int A[], int n) {
+    int curr = A[0];
+    for (int i = 1; i < n; ++i) {
+        if (curr > A[i])
+            return 0;
+        curr = A[i];
+    }
+    return 1;
+}
+
+void insertion_sort(int A[], int n) {
+    for (int i = 1; i < n; ++i) {
+        int temp = A[i];
+        int j = i;
+        while (--j >= 0 && temp < A[j])
+            A[j + 1] = A[j];
+        A[j + 1] = temp;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int A[n];
+
+    for (int i = 0; i < n; ++i)
+        scanf("%d", A + i);
+
+    insertion_sort(A, n);
+
+    if (is_sorted(A, n))
+        printf("SORTED\n");
+    else
+        printf("NOT SORTED\n");
+
+    return 0;
+}
+
+
