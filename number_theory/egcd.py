e134800d113bc716787e2599a9dd06181c753600
Tariq <csetariq@gmail.com>
2016-02-11 15:11:28 +0530

    add debug statements to extended euclidean

--- number_theory/egcd.py
+++ number_theory/egcd.py
@@ -2,11 +2,14 @@ def egcd(a, b):
     if (b == 0):
         return (a, 1, 0)
     else:
+        print "for a:%d b:%d" % (a, b)
         gcd, x, y = egcd(b, a % b)
+        print "got %d %d %d" % (gcd, x, y)
         return (gcd, y, x - (a / b) * y)
 
 def modinv(a, m):
     gcd, x, y = egcd(a, m)
+    print "mod inv got %d %d %d" % (gcd, x, y)
     if gcd != 1:
         return None
     else:
